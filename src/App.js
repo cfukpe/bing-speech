32873328dda3abc641025fa7632ec2e59f03ed98
Chinedu Ukpe <cukpe@emgeresources.com>
2022-05-17 09:44:02 +0100

    Add speech rec

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import {SpeechConfig, AudioConfig, SpeechRecognizer, ResultReason, CancellationDetails, CancellationReason} from 'microsoft-cognitiveservices-speech-sdk';
+
+
+
+function fromFile() {
+    let audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+    // let audioConfig = sdk.AudioConfig.fromWavFileInput(fs.readFileSync("test.wav"));
+    const speechConfig = SpeechConfig.fromSubscription("69433f9ca7694608bc391d7ea53b2296", "eastus");
+    speechConfig.speechRecognitionLanguage = "en-US";
+    let speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+    speechRecognizer.recognizeOnceAsync(result => {
+        switch (result.reason) {
+            case ResultReason.RecognizedSpeech:
+                console.log(`RECOGNIZED: Text=${result.text}`);
+                break;
+            case ResultReason.NoMatch:
+                console.log("NOMATCH: Speech could not be recognized.");
+                break;
+            case ResultReason.Canceled:
+                const cancellation = CancellationDetails.fromResult(result);
+                console.log(`CANCELED: Reason=${cancellation.reason}`);
+
+                if (cancellation.reason == CancellationReason.Error) {
+                    console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
+                    console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
+                    console.log("CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+        }
+        speechRecognizer.close();
+    });
+}
 
 function App() {
+
+  useEffect(() => {
+    // fromFile();
+  },[]);
+
+  const handleAudioChange = e => {
+    const file = e.target.files[0];
+    console.log(file.name)
+    fromFile();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='file' accept='audio/*' capture onChange={handleAudioChange} />
     </div>
   );
 }
